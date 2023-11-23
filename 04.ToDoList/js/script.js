3c71679cc125d755c6ef381fc77fc64af8321d3b
mxnxa <alsgk0428@naver.com>
2023-11-24 00:36:03 +0900

    일정 추가, 출력, 삭제 구현

--- 04.ToDoList/js/script.js
+++ 04.ToDoList/js/script.js
@@ -1,46 +1,85 @@
-// 일정 추가
-// 일정 삭제
-
 const todoList = document.querySelector(".todo_list");
 const todoItem = document.querySelector(".todo_item");
 const todoInput = document.querySelector(".todo_input");
 
-const todos = [
-  { id: 1, content: "할 일1" },
-  { id: 2, content: "할 일2" },
-  { id: 3, content: "할 일3" },
-];
+let todos = [];
+let id = 0;
 
 // create
 todoInput.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
-    paintTodos();
+    if (!todoInput.value) {
+      alert("일정을 입력하세요.");
+      todoInput.focus();
+      return;
+    }
+
+    appendTodos(e.target.value);
+    todoInput.value = "";
+    console.log(todos);
   }
 });
 
-// 일정 출력
-function paintTodos() {
-  const inputValue = todoInput.value;
-
-  if (inputValue === "") {
-    alert("일정을 입력하세요.");
-    todoInput.focus();
-    return false;
-  }
-
-  todos.push(inputValue);
-
-  todoInput.value = "";
-  console.log(todos);
+// 일정 추가
+function appendTodos(text) {
+  const newTodos = todos.concat({ id: id++, content: text, complete: false });
+  //   todos = [...todos, { id: id++, content: todoInput.value, complete: false }];
+  todos = newTodos;
+  paintTodos();
 }
 
-// function appendTodos() {
-//   todos.concat({
-//     id: todos[todos.length - 1].id + 1,
-//     content: todoInput.value,
+// 일정 출력
+function paintTodos() {
+  todoInput.innerHTML = "";
+
+  todos.forEach(function (todo) {
+    const todoItemElem = document.createElement("li");
+    todoItemElem.classList.add("todo_item");
+
+    const checkBtnElem = document.createElement("div");
+    checkBtnElem.classList.add("button");
+    checkBtnElem.classList.add("btn_check");
+
+    const todoTextElem = document.createElement("p");
+    todoTextElem.classList.add("todo_text");
+    todoTextElem.innerHTML = todo.content;
+
+    const updateBtnElem = document.createElement("div");
+    updateBtnElem.classList.add("btn_update");
+    updateBtnElem.innerText = "수정";
+
+    const delBtnElem = document.createElement("div");
+    delBtnElem.classList.add("btn_delete");
+    delBtnElem.innerText = "삭제";
+
+    todoItemElem.appendChild(checkBtnElem);
+    todoItemElem.appendChild(todoTextElem);
+    todoItemElem.appendChild(updateBtnElem);
+    todoItemElem.appendChild(delBtnElem);
+
+    todoList.appendChild(todoItemElem);
+
+    todoInput.value = "";
+
+    delBtnElem.addEventListener("click", (e) => {
+      const target = e.target.parentNode;
+      todoList.removeChild(target);
+    });
+  });
+}
+
+// delBtnElem.addEventListener("click", (e) => {
+//     todos.filter((todo) => todo.id !== e.target.id);
+//     //   paintTodos();
+//     console.log(e.target.id);
+//     console.log(todos);
 //   });
+
+// 일정 수정
+// function updateTodo() {
+//     updateBtnElem.addEventListener("click", e =>
+
+//     )
 // }
 
-// update
-
-// delete
+// 일정 삭제
